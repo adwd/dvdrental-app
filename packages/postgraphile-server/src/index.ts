8f29fd897fdffe89661b6c528d77f1185f010016
masahiro.nishida <masahiro.nishida@zozo.com>
2021-11-08 19:23:51 +0900

    update dependencies

--- packages/postgraphile-server/src/index.ts
+++ packages/postgraphile-server/src/index.ts
@@ -5,6 +5,8 @@ import PgSimplifyInflectorPlugin from "@graphile-contrib/pg-simplify-inflector";
 import PgAggregatesPlugin from "@graphile/pg-aggregates";
 import { myPlugin } from "./my-plugin";
 
+const port = process.env.PORT || 5000;
+
 http
   .createServer(
     postgraphile(
@@ -25,4 +27,8 @@ http
       }
     )
   )
-  .listen(process.env.PORT || 5000);
+  .listen(port);
+
+console.log(
+  `postgraphile server is running: http://localhost:${port}/graphiql`
+);
